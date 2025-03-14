ce2dbd7ff8d5b2adfe970fbc851088c91c49c1c2
Mykola-L <mykola.liashchenko@gmail.com>
2025-03-14 15:30:56 +0200

    add models

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class Author(Base):
+    __tablename__ = "authors"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False, index=True)
+    published_year = Column(Integer, nullable=False, index=True)
+    genre = Column(String, nullable=False, index=True)
+    author_id = Column(Integer, ForeignKey("authors.id"), nullable=False)
+
+    author = relationship("Author", back_populates="books")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
